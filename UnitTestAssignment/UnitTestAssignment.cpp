4e053de5fc9a61515d27a25a84710e9b6c4667e0
Ifeanyi Chiemeke <chiemeke.ifeanyi@gmail.com>
2025-10-19 16:56:05 -0700

    add test cases for width and length adding functionality

--- UnitTestAssignment/UnitTestAssignment.cpp
+++ UnitTestAssignment/UnitTestAssignment.cpp
@@ -3,6 +3,9 @@
 
 extern "C" int getPerimeter(int* length, int* width);
 extern "C" int getArea(int* length, int* width);
+extern "C" void setLength(int input, int* length); 
+extern "C" void setWidth(int input, int* width);
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -33,4 +36,53 @@ namespace UnitTestAssignment
 
 		}
 	};
+    TEST_CLASS(SetLengthTests)
+    {
+    public:
+        TEST_METHOD(SetLength_Within_Range)
+        {
+            int len = 20;
+            setLength(25, &len);
+            Assert::AreEqual(25, len);
+        }
+
+        TEST_METHOD(SetLength_Above_Range)
+        {
+            int len = 13; 
+            setLength(100, &len);
+            Assert::AreEqual(13, len);
+        }
+
+        TEST_METHOD(SetLength_Below_Range)
+        {
+            int len = 7;
+            setLength(0, &len);
+            Assert::AreEqual(7, len);
+        }
+    };
+
+    TEST_CLASS(SetWidthTests)
+    {
+    public:
+        TEST_METHOD(SetWidth_Within_Range)
+        {
+            int width = 0;
+            setWidth(40, &width);
+            Assert::AreEqual(40, width);
+        }
+
+        TEST_METHOD(SetWidth_Below_Range)
+        {
+            int width = 99;
+            setWidth(0, &width);
+            Assert::AreEqual(99, width);
+        }
+
+        TEST_METHOD(SetWidth_Above_Range)
+        {
+            int width = 5;
+            setWidth(100, &width);
+            Assert::AreEqual(5, width);
+        }
+    };
 }
